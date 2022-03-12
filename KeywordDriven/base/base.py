422653954d446607b3a7a70fd63392e45492a09a
a_fei_fei_fei_fei <2928577880@qq.com>
2022-03-13 00:14:52 +0800

    [ADD]增加日志 记录post请求

--- KeywordDriven/base/base.py
+++ KeywordDriven/base/base.py
@@ -1,6 +1,7 @@
 from requests import Session
 from abs_path import config_path
 from public.handler_yaml import YamlClient
+from public.log import Log
 
 
 class BaseClient(object):
@@ -8,4 +9,5 @@ class BaseClient(object):
     Session = Session()
 
     def post(self, url, data=None, json=None, **kwargs):
+        Log.info(f"本次post请求信息为:{url},{data},{json},{kwargs}")
         return self.Session.post(url, verify=False, data=data, json=json, **kwargs)
