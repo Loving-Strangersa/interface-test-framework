05b0c5b0e9ac8f8dc46ffe0b9e7309786a35aa25
a_fei_fei_fei_fei <2928577880@qq.com>
2022-03-13 00:11:55 +0800

    [ADD]增加时间戳,日志文件目录

--- abs_path.py
+++ abs_path.py
@@ -1,7 +1,18 @@
 import os
 
 # 项目根目录
+import time
+
 dir_path = os.path.dirname(os.path.abspath(__file__))
 
 # 配置yaml文件
 config_path = os.path.join(dir_path, "config", "config.yaml")
+
+# 日志文件
+log_path = os.path.join(dir_path, "config", "log")
+
+# 本地时间戳
+time_now = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
+
+
+print(time_now)
