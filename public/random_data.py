913288c8d4bd08d97c6253c891eec79567444db0
a_fei_fei_fei_fei <2928577880@qq.com>
2022-03-14 03:33:29 +0800

    [OPT]增加property装饰器

--- public/random_data.py
+++ public/random_data.py
@@ -6,6 +6,7 @@ class RandomData(object):
     def __init__(self):
         self.faker = Faker(locale='zh_CN')
 
+    @property
     def random_user_agent(self):
         """
         随机生成请求头
@@ -14,6 +15,7 @@ class RandomData(object):
         """
         return self.faker.user_agent()
 
+    @property
     def random_phone(self):
         """
         随机生成电话号码
@@ -22,6 +24,7 @@ class RandomData(object):
         """
         return self.faker.phone_number()
 
+    @property
     def random_name(self):
         """
         随机生成姓名
@@ -30,6 +33,7 @@ class RandomData(object):
         """
         return self.faker.name()
 
+    @property
     def random_email(self):
         """
         随机生成邮箱地址
@@ -40,3 +44,6 @@ class RandomData(object):
 
 
 counterfeit = RandomData()
+
+if __name__ == '__main__':
+    print(counterfeit.random_user_agent)
