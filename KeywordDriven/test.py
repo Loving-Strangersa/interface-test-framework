8548907e532e5633196289d5d871be3ad3c4783c
a_fei_fei_fei_fei <2928577880@qq.com>
2022-03-14 03:34:25 +0800

    [ADD]增加用例文档

--- KeywordDriven/test.py
+++ KeywordDriven/test.py
@@ -7,4 +7,24 @@ def fresh_parse_data():
     :return:
     :rtype:
     """
-    YamlClient.read_yaml("./test")
+    YamlClient.read_yaml("case")
+
+
+def add_attr(**kwargs):
+    """
+    初始化用例级别
+    :param kwargs:
+    :return:
+    """
+    def wrapper(f):
+        if "type" in kwargs:
+            type_list = ["P0", "P1", "P2", "P3"]
+
+            case_type = kwargs.get("type")
+            if isinstance(case_type, str):
+                if case_type not in type_list:
+                    raise ValueError(f"用例级别{case_type}不是允许的用例级别{type_list}")
+
+        return f.__name__
+
+    return wrapper
