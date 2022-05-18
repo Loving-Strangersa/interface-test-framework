b6bf7e57932d5572242209188575b54d0a4ffb07
a_fei_fei_fei_fei <2928577880@qq.com>
2022-05-18 23:06:43 +0800

    [OPT]更新底层api

--- KeywordDriven/api/api.py
+++ KeywordDriven/api/api.py
@@ -1,9 +1,14 @@
-from KeywordDriven.base.base import BaseClient
+from KeywordDriven.base.base_user import BaseClient
 
 
 class ApiClient(BaseClient):
 
-    def inquiry_of_id_card(self, params):
-        api_url = "/UserAction"
-        response = self.post(url=self.host + api_url, data=params)
-        return response.text
+    def login(self, params):
+        api_url = "/api/login"
+        response = self.post(url=self.host + api_url, json=params)
+        return response.json()
+
+    def register(self, params):
+        api_url = "/api/register"
+        response = self.post(url=self.host + api_url, json=params)
+        return response.json()
