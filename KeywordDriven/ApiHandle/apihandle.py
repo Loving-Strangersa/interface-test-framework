c50a9703fea636234a8dceaea72a856493721de7
a_fei_fei_fei_fei <2928577880@qq.com>
2022-05-18 23:06:56 +0800

    [OPT]更新底层api

--- KeywordDriven/ApiHandle/apihandle.py
+++
@@ -1,9 +0,0 @@
-import json
-
-from KeywordDriven.api.api import ApiClient
-
-
-class DataHandle(ApiClient):
-
-    def inquiry_of_id_card_pp(self, params):
-        return json.loads(self.inquiry_of_id_card(params))
