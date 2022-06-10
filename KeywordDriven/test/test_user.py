a64d749ce523db9afbef3a7f525bfa15ca1d83fb
chron <chron@foxmail.com>
2022-06-10 15:44:13 +0800

    [OPT]优化获取数据方法，未完成

--- KeywordDriven/test/test_user.py
+++ KeywordDriven/test/test_user.py
@@ -7,7 +7,7 @@ from KeywordDriven.step.step_user import StepUser
 # TODO 进行step数据串联
 class TestUser(StepUser):
 
-    # @allure.title("用户正常登录")
+    @allure.title("用户正常登录")
     def test_user_login_succeed(self):
         self.step_user_register(selector="step_user_register")
         self.step_user_login(selector="step_user_login")
